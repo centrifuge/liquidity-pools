6c7829f8b053e9c4a73682bd0ea015f401260526
nuno <hi@nunoalexandre.com>
2023-01-30 15:12:32 +0100

    fixup

--- src/Messages.sol
+++ src/Messages.sol
@@ -117,36 +117,6 @@ library ConnectorMessages {
         validUntil = uint64(_msg.indexUint(57, 8));
     }
 
-    // Convert an hexadecimal character to their value
-    function fromHexChar(uint8 c) internal pure returns (uint8) {
-        if (bytes1(c) >= bytes1("0") && bytes1(c) <= bytes1("9")) {
-            return c - uint8(bytes1("0"));
-        }
-        if (bytes1(c) >= bytes1("a") && bytes1(c) <= bytes1("f")) {
-            return 10 + c - uint8(bytes1("a"));
-        }
-        if (bytes1(c) >= bytes1("A") && bytes1(c) <= bytes1("F")) {
-            return 10 + c - uint8(bytes1("A"));
-        }
-        revert("Failed to encode hex char");
-    }
-
-    // Convert an hexadecimal string to raw bytes
-    function fromHex(string memory s) internal pure returns (bytes memory) {
-        bytes memory ss = bytes(s);
-        require(ss.length % 2 == 0); // length must be even
-        bytes memory r = new bytes(ss.length / 2);
-
-        for (uint256 i = 0; i < ss.length / 2; ++i) {
-            r[i] = bytes1(
-                fromHexChar(uint8(ss[2 * i])) *
-                16 +
-                fromHexChar(uint8(ss[2 * i + 1]))
-            );
-        }
-        return r;
-    }
-
     /**
      * Update token price
      * 
