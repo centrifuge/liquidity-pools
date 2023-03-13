2724b02ede4c581d23b314224a8a40d71f7303cd
Jeroen Offerijns <jeroen@offerijns.com>
2023-03-13 14:36:05 +0100

    Change pragmas

--- src/token/memberlist.sol
+++ src/token/memberlist.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.7.6;
+pragma solidity ^0.8.18;
 
 interface MemberlistLike {
     function updateMember(address user, uint256 validUntil) external;
--- src/token/restricted.sol
+++ src/token/restricted.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.7.6;
+pragma solidity ^0.8.18;
 
 import "./erc20.sol";
 
