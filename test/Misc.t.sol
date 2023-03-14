79a4e0ef459017091ac9a99e5025db95a414a653
nuno <hi@nunoalexandre.com>
2023-03-14 09:24:09 +0100

    test: testAddressToBytes32Cast

--- test/Misc.t.sol
+++ test/Misc.t.sol
@@ -11,4 +11,17 @@ contract MiscTest is Test {
     function testCallIndex() public {
         assertEq(abi.encodePacked(uint8(uint256(108)), uint8(uint256(99))), hex"6c63");
     }
+
+    function testAddressToBytes32Cast() public {
+        assertEq(
+            bytes32(abi.encodePacked(hex"1231231231231231231231231231231231231231")),
+            bytes32(hex"1231231231231231231231231231231231231231000000000000000000000000")
+        );
+
+        assertEq(
+            bytes32(abi.encodePacked(hex"1231231231231231231231231231231231231231")),
+            bytes32(abi.encodePacked(hex"1231231231231231231231231231231231231231", bytes(hex"000000000000000000000000")))
+        );
+
+    }
 }
