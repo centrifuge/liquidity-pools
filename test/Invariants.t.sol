baeef4ab8608b1d13218c5269363a1f50f7db3c6
Jeroen Offerijns <Offerijns@users.noreply.github.com>
2023-06-15 20:45:41 +0200

    Investor invariant test (#49)
    * Set up invariant tests
    
    * Fix invariant test setup
    
    * try: Add xcm-transactor precompile as dep
    
    * Make XcmTransactorV1.sol Solidity 0.7.6 compatible
    
    * Add xcmTransactor property to Router.sol
    
    * wip
    
    * wip: Build Multilocation for Centrifuge and CFG
    
    * Impl POC sendMessage
    
    * Set up investor invariant test
    
    * Pass generous transact max weight
    
    The xcmp_queue config max weight is 20B. We pass 5B which is about what
    we pass for CentChain -> Moonbase remote EVM calls through XCM.
    
    * Make all amount uint128
    
    * Minor fixes
    
    * Switch to XcmTransactorV2.sol
    
    * tmp: Drop onlyModifier from sendMessage
    
    * doc
    
    * Set proper feeAmount
    
    * router: Add sendMessageDebug
    
    * Prefix message bytearray with its scale-encoded length
    
    * Add centrifugeChainHandleCallIndex property to Router.sol
    
    * Add XcmWeightsInfo struct
    
    * Update RouterLike interface
    
    * Add xcmWeightInfo property and update function
    
    * Pass XcmWeightInfo fields seperately
    
    * Add message_length_scale_encoded
    
    * fixup
    
    * Drop sendDebug
    
    * Add devTransfer for easy testing
    
    * fixup + drop unused code
    
    * Address TODOs
    
    * Implement File pattern for setting XcmWeightInfo
    
    * Clean up MultiLocation docs
    
    * Add auth modifier and operations
    
    * fixup
    
    * Make centrifugeChainOrigin immutable
    
    * Update src/routers/xcm/Router.sol
    
    * Rename all transfer user params to destinationAddress
    
    * fmt
    
    * Make call index params immutable
    
    * fmt
    
    * Fix Connector.transfer logic
    
    * Doc and extend handleTransfer
    
    * Domain before destinationAddress
    
    * Just burn
    
    * Add MockXcmRouter
    
    * Drop Router from the test setup
    
    Having MockXcmRouter wrapping around the actual Router means that the
    underlying connector contract needs to only auhtorize the actual Router
    to call it but it then needs to call the upper-layer, MockXcmRouter. So
    we drop the actual Router and just use the MockXcmRouter.
    
    * Invariants: Fix setup and re-enable test
    
    * Fix comment
    
    * Fix testTransferToCentrifuge
    
    * Add testTransferToUnsupportedDomain
    
    * Add testTransferToInvalidDomain
    
    * Undo name changing
    
    I needed to make these changes  to help me reason through all the
    nesting going on but now I'm good.
    
    * Drop redundant comments
    
    * fixup
    
    * Drop more comments
    
    * Drop todo
    
    * Fix args
    
    * Fix build
    
    * removed default value of 10 from totalTransferredOut
    
    * add investorSolvency and totalSupply invariants
    
    * change investor.sol to investorManager and add support for multiple investors
    
    * fix import
    
    * forge fmt
    
    ---------
    
    Co-authored-by: nuno <hi@nunoalexandre.com>
    Co-authored-by: Adam Stox <adamstox@protonmail.com>

--- test/Invariants.t.sol
+++ test/Invariants.t.sol
@@ -6,9 +6,11 @@ import {CentrifugeConnector} from "src/Connector.sol";
 import {ConnectorEscrow} from "src/Escrow.sol";
 import {MockHomeConnector} from "./mock/MockHomeConnector.sol";
 import "./mock/MockXcmRouter.sol";
+import {ERC20Like} from "src/token/restricted.sol";
 import {ConnectorGateway} from "src/routers/Gateway.sol";
 import {TrancheTokenFactory, MemberlistFactory} from "src/token/factory.sol";
 import {InvariantPoolManager} from "./accounts/PoolManager.sol";
+import {InvariantInvestor} from "./accounts/InvestorManager.sol";
 import "forge-std/Test.sol";
 import "../src/Connector.sol";
 
@@ -19,6 +21,7 @@ contract ConnectorInvariants is Test {
     ConnectorGateway gateway;
 
     InvariantPoolManager poolManager;
+    InvariantInvestor investor;
 
     address[] private targetContracts_;
 
@@ -37,6 +40,10 @@ contract ConnectorInvariants is Test {
         // Performs random pool and tranches creations
         poolManager = new InvariantPoolManager(connector);
         targetContracts_.push(address(poolManager));
+
+        // Performs random transfers in and out
+        investor = new InvariantInvestor(connector, bridgedConnector);
+        targetContracts_.push(address(investor));
     }
 
     function targetContracts() public view returns (address[] memory) {
@@ -44,12 +51,41 @@ contract ConnectorInvariants is Test {
     }
 
     // Invariant 1: For every tranche that exists, the equivalent pool exists
-    function invariantTrancheRequiresPool() external {
+    function invariant_trancheRequiresPool() external {
         for (uint256 i = 0; i < poolManager.allTranchesLength(); i++) {
             bytes16 trancheId = poolManager.allTranches(i);
             uint64 poolId = poolManager.trancheIdToPoolId(trancheId);
             (, uint256 createdAt) = bridgedConnector.pools(poolId);
             assertTrue(createdAt > 0);
         }
+    }
+
+    // Invariant 2: The tranche token supply should equal the sum of all
+    // transfers in minus the sum of all the transfers out
+    function invariant_tokenSolvency() external {
+        (address token,,,,,) = bridgedConnector.tranches(investor.fixedPoolId(), investor.fixedTrancheId());
+        assertEq(ERC20Like(token).totalSupply(), investor.totalTransferredIn() - investor.totalTransferredOut());
+    }
+
+    // Invariant 3: An investor should not be able to transfer out more tranche tokens than were transferred in
+    function invariant_investorSolvency() external {
+        assertTrue(investor.totalTransferredIn() >= investor.totalTransferredOut());
+        for (uint256 i = 0; i < investor.allInvestorsLength(); i++) {
+            address investorAddress = investor.allInvestors(i);
+            assertTrue(
+                investor.investorTransferredIn(investorAddress) >= investor.investorTransferredOut(investorAddress)
+            );
+        }
+    }
+
+    // Invariant 4: The total supply of tranche tokens should equal the sum of all the investors balances
+    function invariant_totalSupply() external {
+        (address token,,,,,) = bridgedConnector.tranches(investor.fixedPoolId(), investor.fixedTrancheId());
+        uint256 totalSupply = ERC20Like(token).totalSupply();
+        uint256 totalBalance = 0;
+        for (uint256 i = 0; i < investor.allInvestorsLength(); i++) {
+            totalBalance += ERC20Like(token).balanceOf(investor.allInvestors(i));
+        }
+        assertEq(totalSupply, totalBalance);
     }
 }
