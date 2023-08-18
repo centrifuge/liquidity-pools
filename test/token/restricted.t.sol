0c747157991d0fe8fc2d14520bc60998e9e54b17
Jeroen Offerijns <jeroen@offerijns.com>
2023-08-18 13:14:46 +0200

    Fix restricted token tests

--- test/token/restricted.t.sol
+++ test/token/restricted.t.sol
@@ -1,136 +1,134 @@
-// // SPDX-License-Identifier: AGPL-3.0-only
-// pragma solidity ^0.8.18;
-// pragma abicoder v2;
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.18;
+pragma abicoder v2;
 
-// import {TrancheTokenFactory, MemberlistFactory} from "src/token/factory.sol";
-// import {RestrictedTokenLike} from "src/token/restricted.sol";
-// import {MemberlistLike, Memberlist} from "src/token/memberlist.sol";
-// import "forge-std/Test.sol";
+import {RestrictedToken} from "src/token/restricted.sol";
+import {MemberlistLike, Memberlist} from "src/token/memberlist.sol";
+import "forge-std/Test.sol";
 
-// interface ERC20Like {
-//     function balanceOf(address) external view returns (uint256);
-// }
+interface ERC20Like {
+    function balanceOf(address) external view returns (uint256);
+}
 
-// contract RestrictedTokenTest is Test {
-//     RestrictedTokenLike token;
-//     MemberlistLike memberlist;
+contract RestrictedTokenTest is Test {
+    RestrictedToken token;
+    Memberlist memberlist;
 
-//     function setUp() public {
-//         TrancheTokenFactory tokenFactory = new TrancheTokenFactory();
-//         MemberlistFactory memberlistFactory = new MemberlistFactory();
+    function setUp() public {
+        token = new RestrictedToken(18);
+        token.file("name", "Some Token");
+        token.file("symbol", "ST");
 
-//         token = RestrictedTokenLike(tokenFactory.newTrancheToken(1, "1", "Some Token", "ST", 18));
+        memberlist = new Memberlist();
+        token.file("memberlist", address(memberlist));
 
-//         memberlist = MemberlistLike(memberlistFactory.newMemberlist());
-//         token.file("memberlist", address(memberlist));
+        memberlist.updateMember(address(this), type(uint256).max);
+    }
 
-//         memberlist.updateMember(address(this), type(uint256).max);
-//     }
+    // transferFrom
+    function testTransferFromTokensToMemberWorks(uint256 amount, address targetUser, uint256 validUntil) public {
+        vm.assume(baseAssumptions(validUntil, targetUser));
 
-//     // transferFrom
-//     function testTransferFromTokensToMemberWorks(uint256 amount, address targetUser, uint256 validUntil) public {
-//         vm.assume(baseAssumptions(validUntil, targetUser));
+        memberlist.updateMember(targetUser, validUntil);
+        assertEq(memberlist.members(targetUser), validUntil);
 
-//         memberlist.updateMember(targetUser, validUntil);
-//         assertEq(memberlist.members(targetUser), validUntil);
+        token.mint(address(this), amount);
+        token.transferFrom(address(this), targetUser, amount);
+        assertEq(token.balanceOf(targetUser), amount);
+    }
 
-//         token.mint(address(this), amount);
-//         token.transferFrom(address(this), targetUser, amount);
-//         assertEq(token.balanceOf(targetUser), amount);
-//     }
+    function testTransferFromTokensToNonMemberFails(uint256 amount, address targetUser, uint256 validUntil) public {
+        vm.assume(baseAssumptions(validUntil, targetUser));
 
-//     function testTransferFromTokensToNonMemberFails(uint256 amount, address targetUser, uint256 validUntil) public {
-//         vm.assume(baseAssumptions(validUntil, targetUser));
+        token.mint(address(this), amount);
+        vm.expectRevert(bytes("RestrictedToken/not-allowed-to-hold-token"));
+        token.transferFrom(address(this), targetUser, amount);
+        assertEq(token.balanceOf(targetUser), 0);
+    }
 
-//         token.mint(address(this), amount);
-//         vm.expectRevert(bytes("RestrictedToken/not-allowed-to-hold-token"));
-//         token.transferFrom(address(this), targetUser, amount);
-//         assertEq(token.balanceOf(targetUser), 0);
-//     }
+    function testTransferFromTokensToExpiredMemberFails(uint256 amount, address targetUser) public {
+        vm.assume(targetUser != address(0) && targetUser != address(this) && targetUser != address(token));
 
-//     function testTransferFromTokensToExpiredMemberFails(uint256 amount, address targetUser) public {
-//         vm.assume(targetUser != address(0) && targetUser != address(this) && targetUser != address(token));
+        memberlist.updateMember(targetUser, block.timestamp);
+        assertEq(memberlist.members(targetUser), block.timestamp);
 
-//         memberlist.updateMember(targetUser, block.timestamp);
-//         assertEq(memberlist.members(targetUser), block.timestamp);
+        vm.warp(block.timestamp + 1);
 
-//         vm.warp(block.timestamp + 1);
+        token.mint(address(this), amount);
+        vm.expectRevert(bytes("RestrictedToken/not-allowed-to-hold-token"));
+        token.transferFrom(address(this), targetUser, amount);
+        assertEq(token.balanceOf(targetUser), 0);
+    }
 
-//         token.mint(address(this), amount);
-//         vm.expectRevert(bytes("RestrictedToken/not-allowed-to-hold-token"));
-//         token.transferFrom(address(this), targetUser, amount);
-//         assertEq(token.balanceOf(targetUser), 0);
-//     }
+    // Transfer
+    function testTransferTokensToMemberWorks(uint256 amount, address targetUser, uint256 validUntil) public {
+        vm.assume(baseAssumptions(validUntil, targetUser));
 
-//     // Transfer
-//     function testTransferTokensToMemberWorks(uint256 amount, address targetUser, uint256 validUntil) public {
-//         vm.assume(baseAssumptions(validUntil, targetUser));
+        memberlist.updateMember(targetUser, validUntil);
+        assertEq(memberlist.members(targetUser), validUntil);
 
-//         memberlist.updateMember(targetUser, validUntil);
-//         assertEq(memberlist.members(targetUser), validUntil);
+        token.mint(address(this), amount);
+        token.transfer(targetUser, amount);
+        assertEq(token.balanceOf(targetUser), amount);
+    }
 
-//         token.mint(address(this), amount);
-//         token.transfer(targetUser, amount);
-//         assertEq(token.balanceOf(targetUser), amount);
-//     }
+    function testTransferTokensToNonMemberFails(uint256 amount, address targetUser, uint256 validUntil) public {
+        vm.assume(baseAssumptions(validUntil, targetUser));
 
-//     function testTransferTokensToNonMemberFails(uint256 amount, address targetUser, uint256 validUntil) public {
-//         vm.assume(baseAssumptions(validUntil, targetUser));
+        token.mint(address(this), amount);
+        vm.expectRevert(bytes("RestrictedToken/not-allowed-to-hold-token"));
+        token.transfer(targetUser, amount);
+        assertEq(token.balanceOf(targetUser), 0);
+    }
 
-//         token.mint(address(this), amount);
-//         vm.expectRevert(bytes("RestrictedToken/not-allowed-to-hold-token"));
-//         token.transfer(targetUser, amount);
-//         assertEq(token.balanceOf(targetUser), 0);
-//     }
+    function testTransferTokensToExpiredMemberFails(uint256 amount, address targetUser) public {
+        vm.assume(targetUser != address(0) && targetUser != address(this) && targetUser != address(token));
 
-//     function testTransferTokensToExpiredMemberFails(uint256 amount, address targetUser) public {
-//         vm.assume(targetUser != address(0) && targetUser != address(this) && targetUser != address(token));
+        memberlist.updateMember(targetUser, block.timestamp);
+        assertEq(memberlist.members(targetUser), block.timestamp);
 
-//         memberlist.updateMember(targetUser, block.timestamp);
-//         assertEq(memberlist.members(targetUser), block.timestamp);
+        vm.warp(block.timestamp + 1);
 
-//         vm.warp(block.timestamp + 1);
+        token.mint(address(this), amount);
+        vm.expectRevert(bytes("RestrictedToken/not-allowed-to-hold-token"));
+        token.transfer(targetUser, amount);
+        assertEq(token.balanceOf(targetUser), 0);
+    }
 
-//         token.mint(address(this), amount);
-//         vm.expectRevert(bytes("RestrictedToken/not-allowed-to-hold-token"));
-//         token.transfer(targetUser, amount);
-//         assertEq(token.balanceOf(targetUser), 0);
-//     }
+    // Mint
+    function testMintTokensToMemberWorks(uint256 amount, address targetUser, uint256 validUntil) public {
+        vm.assume(baseAssumptions(validUntil, targetUser));
 
-//     // Mint
-//     function testMintTokensToMemberWorks(uint256 amount, address targetUser, uint256 validUntil) public {
-//         vm.assume(baseAssumptions(validUntil, targetUser));
+        memberlist.updateMember(targetUser, validUntil);
+        assertEq(memberlist.members(targetUser), validUntil);
 
-//         memberlist.updateMember(targetUser, validUntil);
-//         assertEq(memberlist.members(targetUser), validUntil);
+        token.mint(targetUser, amount);
+        assertEq(token.balanceOf(targetUser), amount);
+    }
 
-//         token.mint(targetUser, amount);
-//         assertEq(token.balanceOf(targetUser), amount);
-//     }
+    function testMintTokensToNonMemberFails(uint256 amount, address targetUser, uint256 validUntil) public {
+        vm.assume(baseAssumptions(validUntil, targetUser));
 
-//     function testMintTokensToNonMemberFails(uint256 amount, address targetUser, uint256 validUntil) public {
-//         vm.assume(baseAssumptions(validUntil, targetUser));
+        vm.expectRevert(bytes("RestrictedToken/not-allowed-to-hold-token"));
+        token.mint(targetUser, amount);
+        assertEq(token.balanceOf(targetUser), 0);
+    }
 
-//         vm.expectRevert(bytes("RestrictedToken/not-allowed-to-hold-token"));
-//         token.mint(targetUser, amount);
-//         assertEq(token.balanceOf(targetUser), 0);
-//     }
+    function testMintTokensToExpiredMemberFails(uint256 amount, address targetUser) public {
+        vm.assume(targetUser != address(0) && targetUser != address(this) && targetUser != address(token));
 
-//     function testMintTokensToExpiredMemberFails(uint256 amount, address targetUser) public {
-//         vm.assume(targetUser != address(0) && targetUser != address(this) && targetUser != address(token));
+        memberlist.updateMember(targetUser, block.timestamp);
+        assertEq(memberlist.members(targetUser), block.timestamp);
 
-//         memberlist.updateMember(targetUser, block.timestamp);
-//         assertEq(memberlist.members(targetUser), block.timestamp);
+        vm.warp(block.timestamp + 1);
 
-//         vm.warp(block.timestamp + 1);
+        vm.expectRevert(bytes("RestrictedToken/not-allowed-to-hold-token"));
+        token.mint(targetUser, amount);
+        assertEq(token.balanceOf(targetUser), 0);
+    }
 
-//         vm.expectRevert(bytes("RestrictedToken/not-allowed-to-hold-token"));
-//         token.mint(targetUser, amount);
-//         assertEq(token.balanceOf(targetUser), 0);
-//     }
-
-//     function baseAssumptions(uint256 validUntil, address targetUser) internal view returns (bool) {
-//         return validUntil > block.timestamp && targetUser != address(0) && targetUser != address(this)
-//             && targetUser != address(token);
-//     }
-// }
+    function baseAssumptions(uint256 validUntil, address targetUser) internal view returns (bool) {
+        return validUntil > block.timestamp && targetUser != address(0) && targetUser != address(this)
+            && targetUser != address(token);
+    }
+}
