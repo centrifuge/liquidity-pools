78eeac991799e88813c0f299336b0898612c0014
Jeroen Offerijns <jeroen@offerijns.com>
2023-08-18 16:07:17 +0200

    Add broadcast

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 source .env
 
-forge script script/Axelar-EVM.s.sol:AxelarEVMScript --optimize --sizes --rpc-url $RPC_URL --private-key $PRIVATE_KEY --resume --verify --chain-id $CHAIN_ID --etherscan-api-key $ETHERSCAN_KEY -vvvv
+forge script script/Permissionless.s.sol:PermissionlessScript --optimize --sizes --rpc-url $RPC_URL --private-key $PRIVATE_KEY --verify --broadcast --chain-id $CHAIN_ID --etherscan-api-key $ETHERSCAN_KEY -vvvv
