fa09c1785f93daefc7657b670380184411a0b12d
Jeroen Offerijns <jeroen@offerijns.com>
2023-08-23 15:21:51 +0200

    Rewrite deploy script

--- src/token/restricted.sol
+++ src/token/restricted.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.18;
 
-import "./erc20.sol";
+import "./ERC20.sol";
 
 interface MemberlistLike {
     function hasMember(address) external view returns (bool);
