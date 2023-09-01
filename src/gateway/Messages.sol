ef1460eb4112a108446681a18775d7b1a6cf7526
Jeroen Offerijns <jeroen@offerijns.com>
2023-09-01 22:09:29 +0200

    Remove unused code

--- src/gateway/Messages.sol
+++ src/gateway/Messages.sol
@@ -771,10 +771,6 @@ library Messages {
         return abi.encodePacked(uint8(Call.CancelInvestOrder), poolId, trancheId, investor, currency);
     }
 
-    function isCancelInvestOrder(bytes memory _msg) internal pure returns (bool) {
-        return messageType(_msg) == Call.CancelInvestOrder;
-    }
-
     function parseCancelInvestOrder(bytes memory _msg)
         internal
         pure
@@ -792,10 +788,6 @@ library Messages {
         returns (bytes memory)
     {
         return abi.encodePacked(uint8(Call.CancelRedeemOrder), poolId, trancheId, investor, currency);
-    }
-
-    function isCancelRedeemOrder(bytes memory _msg) internal pure returns (bool) {
-        return messageType(_msg) == Call.CancelRedeemOrder;
     }
 
     function parseCancelRedeemOrder(bytes memory _msg)
--- test/gateway/Messages.t.sol
+++ test/gateway/Messages.t.sol
@@ -877,32 +877,6 @@ contract MessagesTest is Test {
         assertEq(Messages.formatDomain(Messages.Domain.EVM, 43114), hex"01000000000000a86a");
     }
 
-    // Convert an hexadecimal character to their value
-    function fromHexChar(uint8 c) internal pure returns (uint8) {
-        if (bytes1(c) >= bytes1("0") && bytes1(c) <= bytes1("9")) {
-            return c - uint8(bytes1("0"));
-        }
-        if (bytes1(c) >= bytes1("a") && bytes1(c) <= bytes1("f")) {
-            return 10 + c - uint8(bytes1("a"));
-        }
-        if (bytes1(c) >= bytes1("A") && bytes1(c) <= bytes1("F")) {
-            return 10 + c - uint8(bytes1("A"));
-        }
-        revert("Failed to encode hex char");
-    }
-
-    // Convert an hexadecimal string to raw bytes
-    function fromHex(string memory s) internal pure returns (bytes memory) {
-        bytes memory ss = bytes(s);
-        require(ss.length % 2 == 0); // length must be even
-        bytes memory r = new bytes(ss.length / 2);
-
-        for (uint256 i = 0; i < ss.length / 2; ++i) {
-            r[i] = bytes1(fromHexChar(uint8(ss[2 * i])) * 16 + fromHexChar(uint8(ss[2 * i + 1])));
-        }
-        return r;
-    }
-
     function stringToBytes32(string memory source) internal pure returns (bytes32 result) {
         bytes memory tempEmptyStringTest = bytes(source);
         if (tempEmptyStringTest.length == 0) {
