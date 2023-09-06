a00a0b435673d5f5ef3d63a7e4d51942daba3050
Adam Stox <adamstox@protonmail.com>
2023-09-06 11:16:35 -0400

    update invariant test

--- test/Invariants.t.sol
+++ test/Invariants.t.sol
@@ -5,6 +5,11 @@ import {TestSetup} from "test/TestSetup.t.sol";
 import {InvariantPoolManager} from "test/accounts/PoolManager.sol";
 import "forge-std/Test.sol";
 
+interface LiquidityPoolLike {
+    function poolId() external returns (uint64);
+    function trancheId() external returns (bytes16);
+}
+
 contract ConnectorInvariants is TestSetup {
     InvariantPoolManager invariantPoolManager;
 
@@ -22,13 +27,17 @@ contract ConnectorInvariants is TestSetup {
         return targetContracts_;
     }
 
-    // Invariant 1: For every tranche that exists, the equivalent pool exists
-    function invariantTrancheRequiresPool() external {
-        for (uint256 i = 0; i < invariantPoolManager.allTranchesLength(); i++) {
-            bytes16 trancheId = invariantPoolManager.allTranches(i);
-            uint64 poolId = invariantPoolManager.trancheIdToPoolId(trancheId);
+    // Invariant 1: For every liquidity pool that exists, the equivalent tranche and pool exists
+    function invariantLiquidityPoolRequiresTrancheAndPool() external {
+        for (uint256 i = 0; i < invariantPoolManager.allLiquidityPoolsLength(); i++) {
+            address liquidityPool = invariantPoolManager.allLiquidityPools(i);
+            uint64 poolId = LiquidityPoolLike(liquidityPool).poolId();
+            bytes16 trancheId = LiquidityPoolLike(liquidityPool).trancheId();
             (,, uint256 createdAt) = poolManager.pools(poolId);
             assertTrue(createdAt > 0);
+            address token = poolManager.getTrancheToken(poolId, trancheId);
+            assertTrue(token != address(0));
+            assertTrue(invariantPoolManager.trancheIdToPoolId(trancheId) == poolId);
         }
     }
 }
