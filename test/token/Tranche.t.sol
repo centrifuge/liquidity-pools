215242e6b6d199613212938fb93a1129363cb8fa
Jeroen Offerijns <jeroen@offerijns.com>
2023-09-06 10:57:26 +0200

    Fix tranche test

--- test/token/Tranche.t.sol
+++ test/token/Tranche.t.sol
@@ -78,6 +78,7 @@ contract TrancheTokenTest is Test {
     function testCheckTrustedForwarderWorks(uint256 validUntil, uint256 amount, address random) public {
         vm.assume(validUntil > block.timestamp);
         vm.assume(amount > 0);
+        vm.assume(random != address(token));
 
         assert(!token.isTrustedForwarder(self));
         // make self trusted forwarder
