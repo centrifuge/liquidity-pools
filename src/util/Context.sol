795169e536e0d4a2abc2ecb8b78b915a77ddc46c
Jeroen Offerijns <jeroen@offerijns.com>
2023-09-21 15:20:37 +0200

    Fix making restriction mgr factory auth

--- src/util/Context.sol
+++ src/util/Context.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-/// @title  ERC1271 Context
+/// @title  ERC2771 Context
 /// @dev    Provides information about the current execution context, including the
 ///         sender of the transaction and its data. While these are generally available
 ///         via msg.sender and msg.data, they should not be accessed in such a direct
