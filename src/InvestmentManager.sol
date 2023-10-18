a24380a4cc298127fadc9e1ed0f972be8967ca63
Jeroen <1748621+hieronx@users.noreply.github.com>
2023-10-18 15:23:33 +0200

    Update src/InvestmentManager.sol
    Co-authored-by: Adam Stox <AStox@users.noreply.github.com>

--- src/InvestmentManager.sol
+++ src/InvestmentManager.sol
@@ -548,7 +548,7 @@ contract InvestmentManager is Auth {
         returns (uint256 trancheTokenAmount)
     {
         InvestmentState storage state = investments[liquidityPool][owner];
-        _processRedeem(investments[liquidityPool][owner], currencyAmount.toUint128(), liquidityPool, receiver, owner);
+        _processRedeem(state, currencyAmount.toUint128(), liquidityPool, receiver, owner);
         trancheTokenAmount =
             uint256(_calculateTrancheTokenAmount(currencyAmount.toUint128(), liquidityPool, state.redeemPrice));
     }
