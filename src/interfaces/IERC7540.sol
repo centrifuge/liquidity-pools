45f374593ec186d61db7e751c3a73a78fa22f5c9
Jeroen <1748621+hieronx@users.noreply.github.com>
2023-10-19 12:12:32 +0200

    Add IERC7540 (#190)
    * Add IERC7540
    
    * Update interface
    
    * Update ERC-20 interfaces
    
    * Fix comment
    
    * Skip slither step

---
+++ src/interfaces/IERC7540.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.21;
+
+import {IERC4626} from "./IERC4626.sol";
+
+/// @title  IERC7540
+/// @dev    Interface of the ERC7540 "Asynchronous Tokenized Vault Standard", as defined in
+///         https://github.com/ethereum/EIPs/blob/2e63f2096b0c7d8388458bb0a03a7ce0eb3422a4/EIPS/eip-7540.md[ERC-7540].
+interface IERC7540 is IERC4626 {
+    event DepositRequest(address indexed sender, address indexed operator, uint256 assets);
+    event RedeemRequest(address indexed sender, address indexed operator, address indexed owner, uint256 shares);
+
+    /**
+     * @dev Transfers assets from msg.sender into the Vault and submits a Request for asynchronous deposit/mint.
+     *
+     * - MUST support ERC-20 approve / transferFrom on asset as a deposit Request flow.
+     * - MUST revert if all of assets cannot be requested for deposit/mint.
+     *
+     * NOTE: most implementations will require pre-approval of the Vault with the Vault's underlying asset token.
+     */
+    function requestDeposit(uint256 assets, address operator) external;
+
+    /**
+     * @dev Returns the amount of requested assets in Pending state for the operator to deposit or mint.
+     *
+     * - MUST NOT include any assets in Claimable state for deposit or mint.
+     * - MUST NOT show any variations depending on the caller.
+     * - MUST NOT revert unless due to integer overflow caused by an unreasonably large input.
+     */
+    function pendingDepositRequest(address operator) external view returns (uint256 assets);
+
+    /**
+     * @dev Assumes control of shares from owner and submits a Request for asynchronous redeem/withdraw.
+     *
+     * - MUST support a redeem Request flow where the control of shares is taken from owner directly
+     *   where msg.sender has ERC-20 approval over the shares of owner.
+     * - MUST revert if all of shares cannot be requested for redeem / withdraw.
+     */
+    function requestRedeem(uint256 shares, address operator, address owner) external;
+
+    /**
+     * @dev Returns the amount of requested shares in Pending state for the operator to redeem or withdraw.
+     *
+     * - MUST NOT include any shares in Claimable state for redeem or withdraw.
+     * - MUST NOT show any variations depending on the caller.
+     * - MUST NOT revert unless due to integer overflow caused by an unreasonably large input.
+     */
+    function pendingRedeemRequest(address operator) external view returns (uint256 shares);
+}
