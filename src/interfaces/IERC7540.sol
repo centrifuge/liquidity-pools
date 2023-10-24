4dd70d8ec61c383f6f7a4c87cfbd6172024e5331
Jeroen Offerijns <jeroen@offerijns.com>
2023-10-24 16:04:54 +0200

    Fix comments

--- src/interfaces/IERC7540.sol
+++ src/interfaces/IERC7540.sol
@@ -5,11 +5,11 @@ import {IERC4626} from "./IERC4626.sol";
 
 interface IERC165 {
     /// @notice Query if a contract implements an interface
-    /// @param interfaceId The interface identifier, as specified in ERC-165
-    /// @dev Interface identification is specified in ERC-165. This function
-    ///  uses less than 30,000 gas.
+    /// @param  interfaceId The interface identifier, as specified in ERC-165
+    /// @dev    Interface identification is specified in ERC-165. This function
+    ///         uses less than 30,000 gas.
     /// @return `true` if the contract implements `interfaceId` and
-    ///  `interfaceId` is not 0xffffffff, `false` otherwise
+    ///         `interfaceId` is not 0xffffffff, `false` otherwise
     function supportsInterface(bytes4 interfaceId) external pure returns (bool);
 }
 
