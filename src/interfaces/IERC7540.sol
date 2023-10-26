0d6140c8db4f1e8a2a8ed25727e566c29dc9fab4
Jeroen <1748621+hieronx@users.noreply.github.com>
2023-10-26 14:32:37 +0200

    Add ERC165 support (#194)
    * fix: add tranche (#180)
    
    * Align approval flow and interface with EIP draft (#178)
    
    * Explore new approval flow
    
    * Fix permit logic
    
    * Allowance checks
    
    * Remove fee-on-transfer support
    
    * Fix revert message
    
    * Fix tests
    
    * Remove unused function from interface
    
    * Fix comment
    
    * Naming
    
    * Remove console
    
    * Remove duplicate transfer from function
    
    * Fix balance check
    
    * Add operator param
    
    * Align naming of view functions
    
    * Address pr comments
    
    * disallow freezing the zero address (#181)
    
    * Fix permit fr with lower asset amounts (#183)
    
    * fix frontrunning with smaller asset amounts with permits
    
    * add test condition
    
    Signed-off-by: ilin <alina@k-f.co>
    
    ---------
    
    Signed-off-by: ilin <alina@k-f.co>
    
    * Make restrictionManagerFactory mutable on PoolManager (#182)
    
    * Make restrictionManagerFactory mutable on PoolManager
    
    * group RestrictionManagerFactory with other public vars
    
    * Preview methods should revert (#186)
    
    * Remove preview methods from investment mgr
    
    * Add comment
    
    * Format
    
    * Update src/InvestmentManager.sol
    
    Co-authored-by: Adam Stox <AStox@users.noreply.github.com>
    
    * Update src/InvestmentManager.sol
    
    Co-authored-by: Adam Stox <AStox@users.noreply.github.com>
    
    * Update src/InvestmentManager.sol
    
    Co-authored-by: Adam Stox <AStox@users.noreply.github.com>
    
    * Update src/InvestmentManager.sol
    
    Co-authored-by: Adam Stox <AStox@users.noreply.github.com>
    
    * Update src/InvestmentManager.sol
    
    Co-authored-by: Adam Stox <AStox@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Adam Stox <AStox@users.noreply.github.com>
    
    * Add IERC7540 (#190)
    
    * Add IERC7540
    
    * Update interface
    
    * Update ERC-20 interfaces
    
    * Fix comment
    
    * Skip slither step
    
    * Fix price calculation issue on decrease executions (#184)
    
    * Add test for failing price conversion on decrease
    
    * Attempt to fix conversions
    
    * Automatically claim deposits on trigger request redeem (#187)
    
    * Automatically claim deposits on trigger request redeem
    
    * trigger redemptions when user still has tokens in escrow (#189)
    
    * trigger redemptions when user still has tokens in escrow
    
    Signed-off-by: ilin <alina@k-f.co>
    
    * fix comments
    
    Signed-off-by: ilin <alina@k-f.co>
    
    * add fixes from review
    
    * add fixes from review
    
    * add fixes from review
    
    ---------
    
    Signed-off-by: ilin <alina@k-f.co>
    
    ---------
    
    Signed-off-by: ilin <alina@k-f.co>
    Co-authored-by: Alina Sinelnikova <alina@centrifuge.io>
    
    * Add ERC165 support
    
    * fix permit (#195)
    
    * fix permit
    
    * consolidate code
    
    * fmt
    
    * remove redundant require from permitted deposit request
    
    * Refactor price logic (#192)
    
    * Refactor price logic
    
    * Remove redundant comments
    
    * Fix comments
    
    * Check transfer restriction on request redeem (#193)
    
    * Check transfer restriction on request redeem
    
    * Fix
    
    * Format
    
    * Freeze check both directions
    
    * Fix test
    
    * Change member checks
    
    * Update checks again
    
    * Change interfaces
    
    * Revert cancel rely if no target is scheduled (#197)
    
    * Check price is newer (#198)
    
    * Fix approve in test setup
    
    * Fix redundant investment mgr deployment
    
    * Update interface style
    
    * Format
    
    * Fix pool manager typo
    
    * Undo
    
    ---------
    
    Signed-off-by: ilin <alina@k-f.co>
    Co-authored-by: Alina Sinelnikova <alina@centrifuge.io>
    Co-authored-by: Adam Stox <AStox@users.noreply.github.com>

--- src/interfaces/IERC7540.sol
+++ src/interfaces/IERC7540.sol
@@ -3,12 +3,27 @@ pragma solidity 0.8.21;
 
 import {IERC4626} from "./IERC4626.sol";
 
-/// @title  IERC7540
-/// @dev    Interface of the ERC7540 "Asynchronous Tokenized Vault Standard", as defined in
-///         https://github.com/ethereum/EIPs/blob/2e63f2096b0c7d8388458bb0a03a7ce0eb3422a4/EIPS/eip-7540.md[ERC-7540].
-interface IERC7540 is IERC4626 {
+/**
+ * @dev Interface of the ERC165 standard, as defined in the
+ * https://eips.ethereum.org/EIPS/eip-165[EIP].
+ *
+ * Implementers can declare support of contract interfaces, which can then be
+ * queried by others.
+ */
+interface IERC165 {
+    /**
+     * @dev Returns true if this contract implements the interface defined by
+     * `interfaceId`. See the corresponding
+     * https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified[EIP section]
+     * to learn more about how these ids are created.
+     *
+     * This function call must use less than 30 000 gas.
+     */
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
+
+interface IERC7540Deposit {
     event DepositRequest(address indexed sender, address indexed operator, uint256 assets);
-    event RedeemRequest(address indexed sender, address indexed operator, address indexed owner, uint256 shares);
 
     /**
      * @dev Transfers assets from msg.sender into the Vault and submits a Request for asynchronous deposit/mint.
@@ -28,6 +43,10 @@ interface IERC7540 is IERC4626 {
      * - MUST NOT revert unless due to integer overflow caused by an unreasonably large input.
      */
     function pendingDepositRequest(address operator) external view returns (uint256 assets);
+}
+
+interface IERC7540Redeem {
+    event RedeemRequest(address indexed sender, address indexed operator, address indexed owner, uint256 shares);
 
     /**
      * @dev Assumes control of shares from owner and submits a Request for asynchronous redeem/withdraw.
@@ -47,3 +66,10 @@ interface IERC7540 is IERC4626 {
      */
     function pendingRedeemRequest(address operator) external view returns (uint256 shares);
 }
+
+/**
+ * @title  IERC7540
+ * @dev    Interface of the ERC7540 "Asynchronous Tokenized Vault Standard", as defined in
+ *         https://github.com/ethereum/EIPs/blob/2e63f2096b0c7d8388458bb0a03a7ce0eb3422a4/EIPS/eip-7540.md[ERC-7540].
+ */
+interface IERC7540 is IERC7540Deposit, IERC7540Redeem, IERC4626, IERC165 {}
