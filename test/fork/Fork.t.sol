ba1b9feda876abb3a3576a08d8c6e82518318a84
Jeroen Offerijns <jeroen@offerijns.com>
2023-12-28 14:09:05 +0100

    Determinism check

--- test/fork/Fork.t.sol
+++ test/fork/Fork.t.sol
@@ -200,6 +200,38 @@ contract ForkTest is Test {
         }
     }
 
+    function testDeterminism() public {
+        if (vm.envOr("FORK_TESTS", false)) {
+            for (uint256 i = 0; i < deployments.length; i++) {
+                if (i == 0) {
+                    i++;
+                    // Skipping ethereum-mainnet for now as the tranche token factory needs an update
+                    // TODO: remove this after migrating ethereum-mainnet
+                    continue;
+                }
+
+                // Read deployment file
+                address root = _get(i, ".contracts.root");
+                address escrow = _get(i, ".contracts.escrow");
+                address trancheTokenFactory = _get(i, ".contracts.trancheTokenFactory");
+                _loadFork(i);
+
+                // Check address
+                assertEq(root, 0x498016d30Cd5f0db50d7ACE329C07313a0420502);
+                assertEq(escrow, 0xd595E1483c507E74E2E6A3dE8e7D08d8f6F74936);
+                assertEq(trancheTokenFactory, 0x2d60cd1527073419423B14666E2D43C1Cf28B152);
+
+                // Check bytecode
+                assertEq(keccak256(root.code), 0x47102707d876a808849bf1be9c8af0e58d889c65918ee3af3a95dd54fa389070);
+                assertEq(keccak256(escrow.code), 0xf54c8ad5a295c7d20a91165917fb20fbcd2952c625696710f8f9a012fcc8e042);
+                assertEq(
+                    keccak256(trancheTokenFactory.code),
+                    0x0f2863a7a7ffa6f3db8a3aab70ded9e170752a36734eca4a0ff19dc52ec5e97b
+                );
+            }
+        }
+    }
+
     function _loadDeployment(string memory name) internal {
         deployments.push(vm.readFile(string.concat(vm.projectRoot(), "/deployments/mainnet/", name, ".json")));
     }
