d564797dd2f74080c5a34cb8682f56ce4fb16a78
Jeroen Offerijns <jeroen@offerijns.com>
2023-12-30 02:09:46 +0100

    Add BytesLib test

---
+++ test/unit/libraries/BytesLib.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.21;
+
+import "forge-std/Test.sol";
+import {BytesLib} from "src/libraries/BytesLib.sol";
+
+contract BytesLibTest is Test {
+    function testSlice(bytes memory data, bytes memory randomStart, bytes memory randomEnd) public {
+        bytes memory value = bytes.concat(bytes.concat(randomStart, abi.encodePacked(data)), randomEnd);
+        assertEq(BytesLib.slice(value, randomStart.length, data.length), data);
+    }
+
+    function testToAddress(address addr, bytes memory randomStart, bytes memory randomEnd) public {
+        bytes memory value = bytes.concat(bytes.concat(randomStart, abi.encodePacked(addr)), randomEnd);
+        assertEq(BytesLib.toAddress(value, randomStart.length), addr);
+    }
+
+    function testToUint8(uint8 number, bytes memory randomStart, bytes memory randomEnd) public {
+        bytes memory value = bytes.concat(bytes.concat(randomStart, abi.encodePacked(number)), randomEnd);
+        assertEq(BytesLib.toUint8(value, randomStart.length), number);
+    }
+
+    function testToUint64(uint64 number, bytes memory randomStart, bytes memory randomEnd) public {
+        bytes memory value = bytes.concat(bytes.concat(randomStart, abi.encodePacked(number)), randomEnd);
+        assertEq(BytesLib.toUint64(value, randomStart.length), number);
+    }
+
+    function testToUint128(uint128 number, bytes memory randomStart, bytes memory randomEnd) public {
+        bytes memory value = bytes.concat(bytes.concat(randomStart, abi.encodePacked(number)), randomEnd);
+        assertEq(BytesLib.toUint128(value, randomStart.length), number);
+    }
+
+    function testToBytes32(bytes32 data, bytes memory randomStart, bytes memory randomEnd) public {
+        bytes memory value = bytes.concat(bytes.concat(randomStart, abi.encodePacked(data)), randomEnd);
+        assertEq(BytesLib.toBytes32(value, randomStart.length), data);
+    }
+
+    function testToBytes16(bytes16 data, bytes memory randomStart, bytes memory randomEnd) public {
+        bytes memory value = bytes.concat(bytes.concat(randomStart, abi.encodePacked(data)), randomEnd);
+        assertEq(BytesLib.toBytes16(value, randomStart.length), data);
+    }
+}
