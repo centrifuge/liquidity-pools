e5e2f6454ec9ea40f67e9e796c3471f7316e74e3
Frederik Gartenmeister <mustermeiszer@posteo.de>
2024-02-06 14:59:53 +0100

    feat: move to test

---
+++ test/integration/LocalRouter.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.21;
+
+import {Deployer, RouterLike} from "../../srcipt/Deployer.sol";
+import {LocalRouter} from "./LocalRouter.sol";
+
+interface LiquidityPoolLike {
+    function requestDeposit(uint256 assets, address owner) external;
+}
+
+// Script to deploy Liquidity Pools with an Axelar router.
+contract LocalRouterScript is Deployer {
+    function setUp() public {}
+
+    function run() public {
+        admin = msg.sender;
+
+        deployInvestmentManager(msg.sender);
+        LocalRouter router = new LocalRouter();
+        wire(address(router));
+        router.file("gateway", address(gateway));
+        router.file("sourceChain", "TestDomain");
+        router.file("sourceAddress", "0x1111111111111111111111111111111111111111");
+
+        giveAdminAccess();
+        removeDeployerAccess(address(router), msg.sender);
+    }
+}
