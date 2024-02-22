50446a2eca8bf2c691949f0562251fc5b125b7ce
Frederik Gartenmeister <mustermeiszer@posteo.de>
2024-02-22 15:11:43 +0100

    Add local router (#238)
    * feat: local router
    
    * fix: build
    
    * feat: move to test
    
    * fix: build
    
    * feat: make script usable
    
    * fix: make message lib internal
    
    * Update test/integration/LocalRouter.sol
    
    * Update test/integration/LocalRouter.sol
    
    * Update test/integration/LocalRouter.sol
    
    * Format
    
    * Rename
    
    ---------
    
    Co-authored-by: Jeroen Offerijns <jeroen@offerijns.com>
    Co-authored-by: Jeroen <1748621+hieronx@users.noreply.github.com>

---
+++ test/integration/LocalRouter.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.21;
+
+import {Deployer, RouterLike} from "../../script/Deployer.sol";
+import {LocalRouter} from "./LocalRouter.sol";
+
+interface LiquidityPoolLike {
+    function requestDeposit(uint256 assets, address owner) external;
+}
+
+// Script to deploy Liquidity Pools with an Axelar router.
+contract LocalRouterScript is Deployer {
+    function setUp() public {}
+
+    function run() public {
+        // NOTE: 0x361c43cd5Fd700923Aae9dED678851a201839fc6 is the H160 of Keyring::Admin in the Centrifuge Chain
+        // repository
+        admin = address(0x361c43cd5Fd700923Aae9dED678851a201839fc6);
+        pausers = [address(0x361c43cd5Fd700923Aae9dED678851a201839fc6)];
+
+        deploy(msg.sender);
+        LocalRouter router = new LocalRouter();
+        wire(address(router));
+        router.file("gateway", address(gateway));
+        router.file("sourceChain", "TestDomain");
+        router.file("sourceAddress", "0x1111111111111111111111111111111111111111");
+
+        giveAdminAccess();
+        removeDeployerAccess(address(router), msg.sender);
+    }
+}
