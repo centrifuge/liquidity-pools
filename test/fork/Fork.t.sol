f8c7b578ec6a832de45b34682a35230caa764b38
Jeroen Offerijns <jeroen@offerijns.com>
2024-03-13 15:57:47 +0100

    Fix warnings

--- test/fork/Fork.t.sol
+++ test/fork/Fork.t.sol
@@ -53,7 +53,6 @@ contract ForkTest is Test {
                 address poolManager = _get(i, ".contracts.poolManager");
                 address gateway = _get(i, ".contracts.gateway");
                 address escrow = _get(i, ".contracts.escrow");
-                address userEscrow = _get(i, ".contracts.userEscrow");
                 address router = _get(i, ".contracts.router");
                 address trancheTokenFactory = _get(i, ".contracts.trancheTokenFactory");
                 address liquidityPoolFactory = _get(i, ".contracts.liquidityPoolFactory");
