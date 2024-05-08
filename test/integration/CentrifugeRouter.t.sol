4c4c0dbe2ad06983271bc23af75790784182e0af
Adam Stox <adamstox@protonmail.com>
2024-05-08 16:36:53 -0400

    add testCFGRouterDepositIntoMultipleVaults

--- test/integration/CentrifugeRouter.t.sol
+++ test/integration/CentrifugeRouter.t.sol
@@ -32,7 +32,8 @@ contract CentrifugeRouterTest is BaseTest {
         cfgRouter.requestDeposit(vault_, amount);
 
         // trigger - deposit order fulfillment
-        (uint128 trancheTokensPayout) = fulfillDepositRequest(vault, amount);
+        uint128 assetId = poolManager.assetToId(address(erc20));
+        (uint128 trancheTokensPayout) = fulfillDepositRequest(vault, assetId, amount);
 
         assertEq(vault.maxMint(self), trancheTokensPayout); // max deposit
         assertEq(vault.maxDeposit(self), amount); // max deposit
@@ -41,7 +42,6 @@ contract CentrifugeRouterTest is BaseTest {
 
         // vm.expectRevert(bytes("LiquidityPool/not-owner-or-endorsed"));
         cfgRouter.claimDeposit(vault_, self); // claim Deposit
-        assertApproxEqAbs(trancheToken.balanceOf(self), trancheTokensPayout, 1);
         assertApproxEqAbs(trancheToken.balanceOf(self), trancheTokensPayout, 1);
         assertApproxEqAbs(trancheToken.balanceOf(address(escrow)), 0, 1);
         assertApproxEqAbs(erc20.balanceOf(address(escrow)), amount, 1);
@@ -69,7 +69,8 @@ contract CentrifugeRouterTest is BaseTest {
         vm.prank(randomUser);
         cfgRouter.executeLockedDepositRequest(vault_, address(this));
 
-        (uint128 trancheTokensPayout) = fulfillDepositRequest(vault, amount);
+        uint128 assetId = poolManager.assetToId(address(erc20));
+        (uint128 trancheTokensPayout) = fulfillDepositRequest(vault, assetId, amount);
 
         assertEq(vault.maxMint(self), trancheTokensPayout);
         assertEq(vault.maxDeposit(self), amount);
@@ -79,7 +80,6 @@ contract CentrifugeRouterTest is BaseTest {
         // Any address should be able to call claimDeposit for an investor
         vm.prank(randomUser);
         cfgRouter.claimDeposit(vault_, self);
-        assertApproxEqAbs(trancheToken.balanceOf(self), trancheTokensPayout, 1);
         assertApproxEqAbs(trancheToken.balanceOf(self), trancheTokensPayout, 1);
         assertApproxEqAbs(trancheToken.balanceOf(address(escrow)), 0, 1);
         assertApproxEqAbs(erc20.balanceOf(address(escrow)), amount, 1);
@@ -96,7 +96,8 @@ contract CentrifugeRouterTest is BaseTest {
         erc20.approve(address(cfgRouter), amount);
         centrifugeChain.updateMember(vault.poolId(), vault.trancheId(), self, type(uint64).max); // add user as member
         cfgRouter.requestDeposit(vault_, amount);
-        (uint128 trancheTokensPayout) = fulfillDepositRequest(vault, amount);
+        uint128 assetId = poolManager.assetToId(address(erc20));
+        (uint128 trancheTokensPayout) = fulfillDepositRequest(vault, assetId, amount);
         TrancheTokenLike trancheToken = TrancheTokenLike(address(vault.share()));
         cfgRouter.claimDeposit(vault_, self);
 
@@ -106,29 +107,81 @@ contract CentrifugeRouterTest is BaseTest {
         (uint128 assetPayout) = fulfillRedeemRequest(vault, trancheTokensPayout);
         assertApproxEqAbs(trancheToken.balanceOf(self), 0, 1);
         assertApproxEqAbs(trancheToken.balanceOf(address(escrow)), 0, 1);
-        assertApproxEqAbs(erc20.balanceOf(address(escrow)), amount, 1);
+        assertApproxEqAbs(erc20.balanceOf(address(escrow)), assetPayout, 1);
         assertApproxEqAbs(erc20.balanceOf(self), 0, 1);
+        cfgRouter.claimRedeem(vault_, self);
+        assertApproxEqAbs(erc20.balanceOf(address(escrow)), 0, 1);
+        assertApproxEqAbs(erc20.balanceOf(self), assetPayout, 1);
     }
 
-    function testCFGRouterRoundTrip() public {}
+    function testCFGRouterDepositIntoMultipleVaults(uint256 amount1, uint256 amount2) public {
+        // If lower than 4 or odd, rounding down can lead to not receiving any tokens
+        amount1 = uint128(bound(amount1, 4, MAX_UINT128));
+        vm.assume(amount1 % 2 == 0);
+        amount2 = uint128(bound(amount2, 4, MAX_UINT128));
+        vm.assume(amount2 % 2 == 0);
 
-    function testCFGRouterDepositIntoMultipleVaults() public {}
+        ERC20 erc20X = _newErc20("X's Dollar", "USDX", 6);
+        ERC20 erc20Y = _newErc20("Y's Dollar", "USDY", 6);
+        vm.label(address(erc20X), "erc20X");
+        vm.label(address(erc20Y), "erc20Y");
+        address vault1_ = deployVault(5, 6, defaultRestrictionSet, "name1", "symbol1", bytes16(bytes("1")), 1, address(erc20X));
+        address vault2_ = deployVault(4, 6, defaultRestrictionSet, "name2", "symbol2", bytes16(bytes("2")), 2, address(erc20Y));
+        ERC7540Vault vault1 = ERC7540Vault(vault1_);
+        ERC7540Vault vault2 = ERC7540Vault(vault2_);
+        vm.label(vault1_, "vault1");
+        vm.label(vault2_, "vault2");
+
+        erc20X.mint(self, amount1);
+        erc20Y.mint(self, amount2);
+
+        erc20X.approve(address(cfgRouter), amount1);
+        erc20Y.approve(address(cfgRouter), amount2);
+
+        centrifugeChain.updateMember(vault1.poolId(), vault1.trancheId(), self, type(uint64).max);
+        centrifugeChain.updateMember(vault2.poolId(), vault2.trancheId(), self, type(uint64).max);
+        cfgRouter.requestDeposit(vault1_, amount1);
+        cfgRouter.requestDeposit(vault2_, amount2);
+
+        // trigger - deposit order fulfillment
+        uint128 assetId1 = poolManager.assetToId(address(erc20X));
+        uint128 assetId2 = poolManager.assetToId(address(erc20Y));
+        (uint128 trancheTokensPayout1) = fulfillDepositRequest(vault1, assetId1, amount1);
+        (uint128 trancheTokensPayout2) = fulfillDepositRequest(vault2, assetId2, amount2);
+
+        assertEq(vault1.maxMint(self), trancheTokensPayout1);
+        assertEq(vault2.maxMint(self), trancheTokensPayout2);
+        assertEq(vault1.maxDeposit(self), amount1);
+        assertEq(vault2.maxDeposit(self), amount2);
+        TrancheTokenLike trancheToken1 = TrancheTokenLike(address(vault1.share()));
+        TrancheTokenLike trancheToken2 = TrancheTokenLike(address(vault2.share()));
+        assertEq(trancheToken1.balanceOf(address(escrow)), trancheTokensPayout1);
+        assertEq(trancheToken2.balanceOf(address(escrow)), trancheTokensPayout2);
+
+        cfgRouter.claimDeposit(vault1_, self);
+        cfgRouter.claimDeposit(vault2_, self);
+        assertApproxEqAbs(trancheToken1.balanceOf(self), trancheTokensPayout1, 1);
+        assertApproxEqAbs(trancheToken2.balanceOf(self), trancheTokensPayout2, 1);
+        assertApproxEqAbs(trancheToken1.balanceOf(address(escrow)), 0, 1);
+        assertApproxEqAbs(trancheToken2.balanceOf(address(escrow)), 0, 1);
+        assertApproxEqAbs(erc20X.balanceOf(address(escrow)), amount1, 1);
+        assertApproxEqAbs(erc20Y.balanceOf(address(escrow)), amount2, 1);
+    }
 
     function testCFGRouterRedeemFromMultipleVaults() public {}
 
     function testMulticallingDepositClaimAndRedeem() public {}
 
     // --- helpers ---
-    function fulfillDepositRequest(ERC7540Vault vault, uint256 amount) public returns (uint128 trancheTokensPayout){
+    function fulfillDepositRequest(ERC7540Vault vault, uint128 assetId, uint256 amount) public returns (uint128 trancheTokensPayout){
         uint128 price = 2 * 10 ** 18;
-        uint128 _currencyId = poolManager.assetToId(address(erc20)); // retrieve currencyId
-        trancheTokensPayout = uint128(amount * 10 ** 18 / price); // trancheTokenPrice = 2$
+        trancheTokensPayout = uint128(amount * 10 ** 18 / price);
         assertApproxEqAbs(trancheTokensPayout, amount / 2, 2);
         centrifugeChain.isFulfilledDepositRequest(
             vault.poolId(),
             vault.trancheId(),
             bytes32(bytes20(self)),
-            _currencyId,
+            assetId,
             uint128(amount),
             trancheTokensPayout,
             uint128(amount)
