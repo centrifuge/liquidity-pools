01a614ce9225a7e2c283fa73e6a41590130c0679
Jeroen Offerijns <jeroen@offerijns.com>
2024-05-21 11:16:01 +0200

    Start removing callbacks

--- src/ERC7540Vault.sol
+++ src/ERC7540Vault.sol
@@ -80,7 +80,7 @@ contract ERC7540Vault is Auth, IERC7540 {
 
     // --- ERC-7540 methods ---
     /// @inheritdoc IERC7540Deposit
-    function requestDeposit(uint256 assets, address receiver, address owner, bytes memory data)
+    function requestDeposit(uint256 assets, address receiver, address owner)
         public
         returns (uint256)
     {
@@ -89,13 +89,6 @@ contract ERC7540Vault is Auth, IERC7540 {
 
         require(manager.requestDeposit(address(this), assets, receiver, owner), "ERC7540Vault/request-deposit-failed");
         SafeTransferLib.safeTransferFrom(asset, owner, address(escrow), assets);
-
-        require(
-            data.length == 0 || receiver.code.length == 0
-                || IERC7540DepositReceiver(receiver).onERC7540DepositReceived(msg.sender, owner, REQUEST_ID, assets, data)
-                    == IERC7540DepositReceiver.onERC7540DepositReceived.selector,
-            "ERC7540Vault/receiver-failed"
-        );
 
         emit DepositRequest(receiver, owner, REQUEST_ID, msg.sender, assets);
         return REQUEST_ID;
@@ -106,14 +99,13 @@ contract ERC7540Vault is Auth, IERC7540 {
     function requestDepositWithPermit(
         uint256 assets,
         address receiver,
-        bytes memory data,
         uint256 deadline,
         uint8 v,
         bytes32 r,
         bytes32 s
     ) external {
         try IERC20Permit(asset).permit(msg.sender, address(this), assets, deadline, v, r, s) {} catch {}
-        requestDeposit(assets, receiver, msg.sender, data);
+        requestDeposit(assets, receiver, msg.sender);
     }
 
     /// @inheritdoc IERC7540Deposit
@@ -127,7 +119,7 @@ contract ERC7540Vault is Auth, IERC7540 {
     }
 
     /// @inheritdoc IERC7540Redeem
-    function requestRedeem(uint256 shares, address receiver, address owner, bytes memory data)
+    function requestRedeem(uint256 shares, address receiver, address owner)
         public
         returns (uint256)
     {
@@ -141,13 +133,6 @@ contract ERC7540Vault is Auth, IERC7540 {
         require(
             AuthTransferLike(share).authTransferFrom(sender, owner, address(escrow), shares),
             "ERC7540Vault/transfer-failed"
-        );
-
-        require(
-            data.length == 0 || receiver.code.length == 0
-                || IERC7540RedeemReceiver(receiver).onERC7540RedeemReceived(msg.sender, owner, REQUEST_ID, shares, data)
-                    == IERC7540RedeemReceiver.onERC7540RedeemReceived.selector,
-            "ERC7540Vault/receiver-failed"
         );
 
         emit RedeemRequest(receiver, owner, REQUEST_ID, msg.sender, shares);
@@ -330,19 +315,19 @@ contract ERC7540Vault is Auth, IERC7540 {
     }
 
     // --- Event emitters ---
-    function emitDepositClaimable(address owner, uint256 assets, uint256 shares) public auth {
+    function onDepositClaimable(address owner, uint256 assets, uint256 shares) public auth {
         emit DepositClaimable(owner, REQUEST_ID, assets, shares);
     }
 
-    function emitRedeemClaimable(address owner, uint256 assets, uint256 shares) public auth {
+    function onRedeemClaimable(address owner, uint256 assets, uint256 shares) public auth {
         emit RedeemClaimable(owner, REQUEST_ID, assets, shares);
     }
 
-    function emitCancelDepositClaimable(address owner, uint256 assets) public auth {
+    function onCancelDepositClaimable(address owner, uint256 assets) public auth {
         emit CancelDepositClaimable(owner, REQUEST_ID, assets);
     }
 
-    function emitCancelRedeemClaimable(address owner, uint256 shares) public auth {
+    function onCancelRedeemClaimable(address owner, uint256 shares) public auth {
         emit CancelRedeemClaimable(owner, REQUEST_ID, shares);
     }
 
