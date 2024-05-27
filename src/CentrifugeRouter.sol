f63a401d09c5477c9f0ab6717a38ad24ff395edc
Jeroen Offerijns <jeroen@offerijns.com>
2024-05-27 21:16:16 +0200

    Fix

--- src/CentrifugeRouter.sol
+++ src/CentrifugeRouter.sol
@@ -7,24 +7,29 @@ import {SafeTransferLib} from "src/libraries/SafeTransferLib.sol";
 import {ICentrifugeRouter} from "src/interfaces/ICentrifugeRouter.sol";
 
 contract CentrifugeRouter is Auth, ICentrifugeRouter {
+    /// @inheritdoc ICentrifugeRouter
     mapping(address user => mapping(address vault => uint256 amount)) public lockedRequests;
 
     // --- Administration ---
+    /// @inheritdoc ICentrifugeRouter
     function recoverTokens(address token, address to, uint256 amount) external auth {
         SafeTransferLib.safeTransfer(token, to, amount);
     }
 
     // --- Deposit ---
+    /// @inheritdoc ICentrifugeRouter
     function requestDeposit(address vault, uint256 amount) external {
         IERC7540(vault).requestDeposit(amount, msg.sender, msg.sender);
     }
 
+    /// @inheritdoc ICentrifugeRouter
     function lockDepositRequest(address vault, uint256 amount) external {
         SafeTransferLib.safeTransferFrom(IERC7540(vault).asset(), msg.sender, address(this), amount);
         lockedRequests[msg.sender][vault] += amount;
         emit LockDepositRequest(vault, msg.sender, amount);
     }
 
+    /// @inheritdoc ICentrifugeRouter
     function unlockDepositRequest(address vault) external {
         uint256 lockedRequest = lockedRequests[msg.sender][vault];
         require(lockedRequest > 0, "CentrifugeRouter/user-has-no-locked-balance");
@@ -33,6 +38,7 @@ contract CentrifugeRouter is Auth, ICentrifugeRouter {
         emit UnlockDepositRequest(vault, msg.sender);
     }
 
+    /// @inheritdoc ICentrifugeRouter
     function executeLockedDepositRequest(address vault, address user) external {
         uint256 lockedRequest = lockedRequests[user][vault];
         require(lockedRequest > 0, "CentrifugeRouter/user-has-no-balance");
@@ -41,6 +47,7 @@ contract CentrifugeRouter is Auth, ICentrifugeRouter {
         emit ExecuteLockedDepositRequest(vault, user);
     }
 
+    /// @inheritdoc ICentrifugeRouter
     function claimDeposit(address vault, address user) external {
         uint256 maxDeposit = IERC7540(vault).maxDeposit(user);
         require(maxDeposit > 0, "CentrifugeRouter/user-has-no-balance-to-claim");
@@ -48,10 +55,12 @@ contract CentrifugeRouter is Auth, ICentrifugeRouter {
     }
 
     // --- Redeem ---
+    /// @inheritdoc ICentrifugeRouter
     function requestRedeem(address vault, uint256 amount) external {
         IERC7540(vault).requestRedeem(amount, msg.sender, msg.sender);
     }
 
+    /// @inheritdoc ICentrifugeRouter
     function claimRedeem(address vault, address user) external {
         uint256 maxRedeem = IERC7540(vault).maxRedeem(user);
         require(maxRedeem > 0, "CentrifugeRouter/user-has-no-balance-to-claim");
