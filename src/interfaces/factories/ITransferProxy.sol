0d2f7962836e8790a491d67eec4d784f060e12e5
Jeroen Offerijns <jeroen@offerijns.com>
2024-05-27 21:36:22 +0200

    Remove transfer proxy recovery

--- src/interfaces/factories/ITransferProxy.sol
+++ src/interfaces/factories/ITransferProxy.sol
@@ -4,16 +4,12 @@ pragma solidity >=0.5.0;
 interface ITransferProxy {
     /// @dev Anyone can transfer tokens.
     function transfer(address asset, uint128 amount) external;
-
-    /// @dev The recoverer can receive tokens back. This is not permissionless as this could lead
-    ///      to griefing issues, where tokens are recovered before being transferred out.
-    function recover(address asset, uint128 amount) external;
 }
 
 interface ITransferProxyFactory {
-    /// @dev Lookup proxy, where id = keccak256(destination + recoverer)
-    function proxies(bytes32 id) external view returns (address proxy);
+    /// @dev Lookup proxy by destination address
+    function proxies(bytes32 destination) external view returns (address proxy);
 
     /// @dev Deploy new transfer proxy
-    function newTransferProxy(bytes32 destination, address recoverer) external returns (address);
+    function newTransferProxy(bytes32 destination) external returns (address);
 }
