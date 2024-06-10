197ca6934cb992df9b5a45e5a013ab8013a0e96d
John <bulk.yard@protonmail.com>
2024-06-10 17:54:58 +0200

    chore:(GasService) Remove unnecessary variables

--- src/gateway/GasService.sol
+++ src/gateway/GasService.sol
@@ -9,8 +9,6 @@ import {MessagesLib} from "src/libraries/MessagesLib.sol";
 contract GasService is IGasService, Auth {
     using MathLib for uint256;
 
-    uint80 roundId;
-    uint256 updatedTime;
     /// @inheritdoc IGasService
     uint256 public price;
     /// @inheritdoc IGasService
@@ -22,11 +20,9 @@ contract GasService is IGasService, Auth {
         messageCost = messageCost_;
         proofCost = proofCost_;
         price = price_;
-        updatedTime = block.timestamp;
-        roundId = 1;
     }
-
     /// @inheritdoc IGasService
+
     function file(bytes32 what, uint256 value) external auth {
         if (what == "messageCost") messageCost = value;
         if (what == "proofCost") proofCost = value;
@@ -37,8 +33,6 @@ contract GasService is IGasService, Auth {
     /// @inheritdoc IGasService
     function updatePrice(uint256 value) external auth {
         price = value;
-        roundId++;
-        updatedTime = block.timestamp;
     }
 
     /// @inheritdoc IGasService
