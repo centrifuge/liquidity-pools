5e890a1a6e52c7316cd6cb2fd2b06ef90ae11113
Jeroen Offerijns <jeroen@offerijns.com>
2024-06-21 09:40:54 +0200

    Undo router values

--- src/gateway/routers/axelar/Router.sol
+++ src/gateway/routers/axelar/Router.sol
@@ -34,10 +34,10 @@ interface AxelarGasServiceLike {
 /// @notice Routing contract that integrates with an Axelar Gateway
 
 contract AxelarRouter is IRouter, Auth {
-    string public constant CENTRIFUGE_ID = "centrifuge-2";
-    bytes32 public constant CENTRIFUGE_ID_HASH = keccak256(bytes("centrifuge-2"));
+    string public constant CENTRIFUGE_ID = "centrifuge";
+    bytes32 public constant CENTRIFUGE_ID_HASH = keccak256(bytes("centrifuge"));
     bytes32 public constant CENTRIFUGE_ADDRESS_HASH = keccak256(bytes("0x7369626CEF070000000000000000000000000000"));
-    string public constant CENTRIFUGE_AXELAR_EXECUTABLE = "0x1940aafa3b20d9bfc49dc8f983ba2fe9fa958eaa";
+    string public constant CENTRIFUGE_AXELAR_EXECUTABLE = "0xc1757c6A0563E37048869A342dF0651b9F267e41";
 
     GatewayLike public immutable gateway;
     AxelarGatewayLike public immutable axelarGateway;
