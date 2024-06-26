dc5e843159b6ef8d4b6d876e055b7432e57e1ff5
Frederik Gartenmeister <mustermeiszer@posteo.de>
2024-06-26 21:58:38 +0200

    fix: add fields and fix deploy script

--- test/integration/PassthroughRouter.sol
+++ test/integration/PassthroughRouter.sol
@@ -24,18 +24,21 @@ contract PassthroughRouter is Auth {
     bytes32 internal constant FAKE_COMMAND_ID = keccak256("FAKE_COMMAND_ID");
 
     GatewayLike public gateway;
+    string public sourceChain;
+    string public sourceAddress;
 
     event Route(string destinationChain, string destinationContractAddress, bytes payload);
     event ExecuteOnDomain(string sourceChain, string sourceAddress, bytes payload);
     event ExecuteOnCentrifuge(string sourceChain, string sourceAddress, bytes payload);
     event File(bytes32 indexed what, address addr);
+    event File(bytes32 indexed what, string data);
 
     constructor() {
         wards[msg.sender] = 1;
         emit Rely(msg.sender);
     }
 
-    function file(bytes32 what, address addr) external {
+    function file(bytes32 what, address addr) external auth {
         if (what == "gateway") {
             gateway = GatewayLike(addr);
         } else {
@@ -43,6 +46,18 @@ contract PassthroughRouter is Auth {
         }
 
         emit File(what, addr);
+    }
+
+    function file(bytes32 what, string calldata data) external auth {
+        if (what == "sourceChain") {
+            sourceChain = data;
+        } else if (what == "sourceAddress") {
+            sourceAddress = data;
+        } else {
+            revert("LocalRouter/file-unrecognized-param");
+        }
+
+        emit File(what, data);
     }
 
     /// @notice From Centrifuge to LP on other domain. Just emits an event.
@@ -58,27 +73,27 @@ contract PassthroughRouter is Auth {
     /// @notice From other domain to Centrifuge. Just emits an event.
     ///         Just used on EVM domains.
     function send(bytes calldata message) public {
-        emit Route("LP-EVM-Domain", "Passthrough-Contract", message);
+        emit Route(sourceChain, sourceAddress, message);
     }
 
     /// @notice Execute message on centrifuge
-    function executeOnCentrifuge(string calldata sourceChain, string calldata sourceAddress, bytes calldata payload)
+    function executeOnCentrifuge(string calldata _sourceChain, string calldata _sourceAddress, bytes calldata payload)
         external
     {
         PrecompileLike precompile = PrecompileLike(PRECOMPILE);
-        precompile.execute(FAKE_COMMAND_ID, sourceChain, sourceAddress, payload);
+        precompile.execute(FAKE_COMMAND_ID, _sourceChain, _sourceAddress, payload);
 
-        emit ExecuteOnCentrifuge(sourceChain, sourceAddress, payload);
+        emit ExecuteOnCentrifuge(_sourceChain, _sourceAddress, payload);
     }
 
     /// @notice Execute message on other domain
     function executeOnDomain(
-        string calldata sourceChain,
-        string calldata sourceAddress,
+        string calldata _sourceChain,
+        string calldata _sourceAddress,
         bytes calldata payload
     ) external {
         gateway.handle(payload);
-        emit ExecuteOnDomain(sourceChain, sourceAddress, payload);
+        emit ExecuteOnDomain(_sourceChain, _sourceAddress, payload);
     }
 
     // Added to be ignored in coverage report
