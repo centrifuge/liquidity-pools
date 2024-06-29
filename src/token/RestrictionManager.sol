e899d04a3ded9c57498c69daacd0e8d84d7ee6c5
Jeroen Offerijns <jeroen@offerijns.com>
2024-06-29 22:32:48 +0200

    Clarify comment

--- src/token/RestrictionManager.sol
+++ src/token/RestrictionManager.sol
@@ -18,7 +18,7 @@ import {RestrictionUpdate, IRestrictionManager} from "src/interfaces/token/IRest
 ///         * Supports freezing accounts which blocks transfers both to and from them
 ///         * Allows authTransferFrom calls
 ///
-/// @dev    The first 8 bytes of hookData is used for the memberlist valid until date,
+/// @dev    The first 8 bytes (uint64) of hookData is used for the memberlist valid until date,
 ///         the last bit is used to denote whether the account is frozen.
 contract RestrictionManager is Auth, IRestrictionManager, IHook {
     using BitmapLib for *;
