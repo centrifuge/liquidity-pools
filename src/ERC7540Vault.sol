a1e1d923cd00c126618812cf093d244bfc9edecf
Jeroen Offerijns <jeroen@offerijns.com>
2024-07-01 09:15:47 +0200

    Change requestRedeem sender

--- src/ERC7540Vault.sol
+++ src/ERC7540Vault.sol
@@ -120,14 +120,15 @@ contract ERC7540Vault is Auth, IERC7540Vault {
     /// @inheritdoc IERC7540Redeem
     function requestRedeem(uint256 shares, address controller, address owner) public returns (uint256) {
         require(ITrancheToken(share).balanceOf(owner) >= shares, "ERC7540Vault/insufficient-balance");
-        require(
-            manager.requestRedeem(address(this), shares, controller, owner, msg.sender),
-            "ERC7540Vault/request-redeem-failed"
-        );
 
         // If msg.sender is operator of owner, the transfer is executed as if
         // the sender is the owner, to bypass the allowance check
         address sender = isOperator[owner][msg.sender] ? owner : msg.sender;
+
+        require(
+            manager.requestRedeem(address(this), shares, controller, owner, sender),
+            "ERC7540Vault/request-redeem-failed"
+        );
 
         try ITrancheToken(share).authTransferFrom(sender, owner, address(escrow), shares) returns (bool) {}
         catch {
