c20e1e0d7acfb5259d6badb0c30107533036f0c7
John <bulk.yard@protonmail.com>
2024-07-04 17:00:04 +0200

    chore: Add more CentrifugeRouter unit tests

--- test/mocks/MockERC20Wrapper.sol
+++ test/mocks/MockERC20Wrapper.sol
@@ -7,12 +7,15 @@ import "forge-std/Test.sol";
 
 contract MockERC20Wrapper is ERC20, IERC20Wrapper {
     address public underlying;
+    bool shouldDepositFail;
+    bool shouldWithdrawFail;
 
     constructor(address underlying_) ERC20(IERC20Metadata(underlying_).decimals()) {
         underlying = underlying_;
     }
 
     function depositFor(address account, uint256 value) external returns (bool) {
+        if (shouldDepositFail) return false;
         require(
             IERC20Metadata(underlying).transferFrom(msg.sender, address(this), value),
             "MockERC20Wrapper/failed-transfer"
@@ -27,10 +30,17 @@ contract MockERC20Wrapper is ERC20, IERC20Wrapper {
     }
 
     function withdrawTo(address account, uint256 value) external returns (bool) {
+        if (shouldWithdrawFail) return false;
         balances[msg.sender] -= value;
         totalSupply = totalSupply - value;
 
         IERC20Metadata(underlying).transfer(account, value);
         return true;
+    }
+
+    function shouldFail(bytes32 action, bool value) external {
+        if (action == "deposit") shouldDepositFail = value;
+        else if (action == "withdraw") shouldWithdrawFail = value;
+        else revert("Nothing to fail but yourself!");
     }
 }
--- test/unit/CentrifugeRouter.t.sol
+++ test/unit/CentrifugeRouter.t.sol
@@ -4,8 +4,23 @@ pragma solidity 0.8.26;
 import "test/BaseTest.sol";
 import "src/interfaces/IERC7575.sol";
 import "src/interfaces/IERC7540.sol";
+import {MockERC20Wrapper} from "test/mocks/MockERC20Wrapper.sol";
+
+contract ERC20WrapperFake {
+    address public underlying;
+
+    constructor(address underlying_) {
+        underlying = underlying_;
+    }
+}
 
 contract CentrifugeRouterTest is BaseTest {
+    function testInitialization() public {
+        assertEq(address(router.escrow()), address(routerEscrow));
+        assertEq(address(router.gateway()), address(gateway));
+        assertEq(address(router.poolManager()), address(poolManager));
+    }
+
     function testGetVault() public {
         address vault_ = deploySimpleVault();
         ERC7540Vault vault = ERC7540Vault(vault_);
@@ -38,5 +53,95 @@ contract CentrifugeRouterTest is BaseTest {
         router.lockDepositRequest(vault_, amount, self, self);
 
         assertEq(erc20.balanceOf(address(routerEscrow)), amount);
+    }
+
+    function testUnlockDepositRequests() public {
+        address vault_ = deploySimpleVault();
+        vm.label(vault_, "vault");
+
+        uint256 amount = 100 * 10 ** 18;
+
+        erc20.mint(self, amount);
+        erc20.approve(address(router), amount);
+
+        vm.expectRevert(bytes("CentrifugeRouter/user-has-no-locked-balance"));
+        router.unlockDepositRequest(vault_, self);
+
+        router.lockDepositRequest(vault_, amount, self, self);
+        assertEq(erc20.balanceOf(address(routerEscrow)), amount);
+        assertEq(erc20.balanceOf(self), 0);
+        router.unlockDepositRequest(vault_, self);
+        assertEq(erc20.balanceOf(address(routerEscrow)), 0);
+        assertEq(erc20.balanceOf(self), amount);
+    }
+
+    function testOpenAndClose() public {
+        address vault_ = deploySimpleVault();
+        vm.label(vault_, "vault");
+
+        assertFalse(router.opened(self, vault_));
+        router.open(vault_);
+        assertTrue(router.opened(self, vault_));
+        router.close(vault_);
+        assertFalse(router.opened(self, vault_));
+    }
+
+    function testWrap() public {
+        uint256 amount = 150 * 10 ** 18;
+        uint256 balance = 100 * 10 ** 18;
+        address receiver = makeAddr("receiver");
+        MockERC20Wrapper wrapper = new MockERC20Wrapper(address(erc20));
+
+        vm.expectRevert(bytes("CentrifugeRouter/invalid-owner"));
+        router.wrap(address(wrapper), amount, receiver, makeAddr("ownerIsNeitherCallerNorRouter"));
+
+        vm.expectRevert(bytes("CentrifugeRouter/zero-balance"));
+        router.wrap(address(wrapper), amount, receiver, self);
+
+        erc20.mint(self, balance);
+        erc20.approve(address(router), amount);
+        wrapper.shouldFail("deposit", true);
+        vm.expectRevert(bytes("CentrifugeRouter/deposit-for-failed"));
+        router.wrap(address(wrapper), amount, receiver, self);
+
+        wrapper.shouldFail("deposit", false);
+        router.wrap(address(wrapper), amount, receiver, self);
+        assertEq(wrapper.balanceOf(receiver), balance);
+        assertEq(erc20.balanceOf(self), 0);
+
+        erc20.mint(address(router), balance);
+        router.wrap(address(wrapper), amount, receiver, address(router));
+        assertEq(wrapper.balanceOf(receiver), 200 * 10 ** 18);
+        assertEq(erc20.balanceOf(address(router)), 0);
+    }
+
+    function testUnwrap() public {
+        uint256 amount = 150 * 10 ** 18;
+        uint256 balance = 100 * 10 ** 18;
+        MockERC20Wrapper wrapper = new MockERC20Wrapper(address(erc20));
+        erc20.mint(self, balance);
+        erc20.approve(address(router), amount);
+
+        vm.expectRevert(bytes("CentrifugeRouter/zero-balance"));
+        router.unwrap(address(wrapper), amount, self);
+
+        router.wrap(address(wrapper), amount, address(router), self);
+        wrapper.shouldFail("withdraw", true);
+        vm.expectRevert(bytes("CentrifugeRouter/withdraw-to-failed"));
+        router.unwrap(address(wrapper), amount, self);
+        wrapper.shouldFail("withdraw", false);
+
+        assertEq(wrapper.balanceOf(address(router)), balance);
+        assertEq(erc20.balanceOf(self), 0);
+        router.unwrap(address(wrapper), amount, self);
+        assertEq(wrapper.balanceOf(address(router)), 0);
+        assertEq(erc20.balanceOf(self), balance);
+    }
+
+    function testEstimate() public {
+        bytes memory message = "IRRELEVANT";
+        uint256 estimated = router.estimate(message);
+        (, uint256 gatewayEstimated) = gateway.estimate(message);
+        assertEq(estimated, gatewayEstimated);
     }
 }
