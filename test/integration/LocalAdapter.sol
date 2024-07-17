4d1d6f7cc5bf5022a83333697ee5040b620c4bdc
Jeroen <1748621+hieronx@users.noreply.github.com>
2024-07-17 09:57:38 +0200

    Fix local adapter (#381)

--- test/integration/LocalAdapter.sol
+++ test/integration/LocalAdapter.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.26;
 
 import {Auth} from "./../../src/Auth.sol";
+import {IAdapter} from "src/interfaces/gateway/IAdapter.sol";
 
 interface PrecompileLike {
     function execute(
@@ -19,7 +20,7 @@ interface GatewayLike {
 /// @title  Local Adapter
 /// @notice Routing contract that routes from Substrate to EVM and back.
 ///         I.e. for testing LP in a local Centrifuge Chain deployment.
-contract LocalAdapter is Auth {
+contract LocalAdapter is Auth, IAdapter {
     address internal constant PRECOMPILE = 0x0000000000000000000000000000000000000800;
     bytes32 internal constant FAKE_COMMAND_ID = keccak256("FAKE_COMMAND_ID");
 
@@ -33,6 +34,7 @@ contract LocalAdapter is Auth {
     event File(bytes32 indexed what, address addr);
     event File(bytes32 indexed what, string data);
 
+    // --- Administrative ---
     function file(bytes32 what, address data) external {
         if (what == "gateway") {
             gateway = GatewayLike(data);
@@ -55,6 +57,7 @@ contract LocalAdapter is Auth {
         emit File(what, data);
     }
 
+    // --- Incoming ---
     // From Centrifuge to LP on Centrifuge (faking other domain)
     function callContract(
         string calldata destinationChain,
@@ -65,7 +68,9 @@ contract LocalAdapter is Auth {
         emit RouteToDomain(destinationChain, destinationContractAddress, payload);
     }
 
-    // From LP on Centrifuge (faking other domain) to Centrifuge
+    // --- Outgoing ---
+    /// @inheritdoc IAdapter
+    /// @dev From LP on Centrifuge (faking other domain) to Centrifuge
     function send(bytes calldata message) public {
         PrecompileLike precompile = PrecompileLike(PRECOMPILE);
         precompile.execute(FAKE_COMMAND_ID, sourceChain, sourceAddress, message);
@@ -73,6 +78,13 @@ contract LocalAdapter is Auth {
         emit RouteToCentrifuge(FAKE_COMMAND_ID, sourceChain, sourceAddress, message);
     }
 
-    // Added to be ignored in coverage report
-    function test() public {}
+    /// @inheritdoc IAdapter
+    function estimate(bytes calldata payload, uint256 baseCost) external view returns (uint256) {
+        return 0;
+    }
+
+    /// @inheritdoc IAdapter
+    function pay(bytes calldata payload, address refund) public payable {
+        return;
+    }
 }
