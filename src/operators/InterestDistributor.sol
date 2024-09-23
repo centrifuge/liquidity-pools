44dbe840a4ef0edafe9b57c8e61d3da5dd3e72e4
Jeroen Offerijns <jeroen@offerijns.com>
2024-09-23 14:34:02 +0200

    Use ternary operator

--- src/operators/InterestDistributor.sol
+++ src/operators/InterestDistributor.sol
@@ -42,13 +42,12 @@ contract InterestDistributor is IInterestDistributor {
             poolManager.getTranchePrice(vault_.poolId(), vault_.trancheId(), asset);
         uint128 currentShares = IERC20(vault_.share()).balanceOf(controller).toUint128();
 
-        uint128 request;
-        if (priceLastUpdated > user.lastUpdate) {
-            // Calculate request before updating user.shares, so it is based on the balance at the last price update.
-            // Assuming price updates coincide with epoch fulfillments, this results in only requesting
-            // interest on the previous outstanding balance before the new fulfillment.
-            request = _computeRequest(user.shares, currentShares, user.peak, uint96(currentPrice));
-        }
+        // Calculate request before updating user.shares, so it is based on the balance at the last price update.
+        // Assuming price updates coincide with epoch fulfillments, this results in only requesting
+        // interest on the previous outstanding balance before the new fulfillment.
+        uint128 request = priceLastUpdated > user.lastUpdate
+            ? _computeRequest(user.shares, currentShares, user.peak, uint96(currentPrice))
+            : 0;
 
         user.lastUpdate = uint32(priceLastUpdated);
         if (currentPrice > user.peak) user.peak = uint96(currentPrice);
